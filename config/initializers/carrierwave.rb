1450a21e2972225d9377385d674600770f09ae9f
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2016-08-11 10:47:48 +0100

    Updated aws bucket location

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-      region: 'eu-west-1'
+      region: 'us-west-2'
     }
-    config.fog_directory =ENV['AWS_BUCKET_NAME']
+    config.fog_directory = ENV['AWS_BUCKET_NAME']
 
   end
 end
