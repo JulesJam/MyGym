4031e3b5e037c12913d44d1a88f25e964876d674
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2016-11-17 17:49:33 +0000

    Adding fog

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 gem 'rmagick'
 gem 'rqrcode-with-patches', '~> 0.5.4'
 gem 'carrierwave', '0.10.0'
-gem 'fog', require: 'fog/aws'
+gem 'fog', '1.38.0'
 gem 'ideal_postcodes'
 gem 'devise', '~> 4.2.0'
 gem 'rails', '~> 5.0.0'
@@ -32,6 +32,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,11 @@
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
-      :region                => 'eu-west-1'
-    }
-    config.storage = :fog
-    config.fog_directory = ENV['AWS_BUCKET_NAME']
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
-  end
+  config.fog_credentials = {
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
+    :region                => 'eu-west-1'
+  }
+  config.storage = :fog
+  config.fog_directory = ENV['AWS_BUCKET_NAME']
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
